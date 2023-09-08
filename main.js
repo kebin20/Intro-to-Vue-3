721251089692062f95b38fb5c4b649ec057f20b2
kebin20 <ktanzyl@hotmail.com>
2023-09-08 14:54:34 +0900

    initial setup

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+// eslint-disable-next-line no-undef
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Boots",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam non animi, eos perferendis sequi cupiditate illo quas. Et vero at dolores doloremque maiores veritatis eos recusandae, nesciunt, totam modi odio.",
+    };
+  },
+});
